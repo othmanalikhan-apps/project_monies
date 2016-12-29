c840eab355aa615260248636e0ebd6a7f0b56c64
OthmanEmpire <oz.alikhan@gmail.com>
2016-12-29 06:52:28 +0000

    Factor long strings in santander.py to 'setUp' function for clarity

--- test/unit/test_santander.py
+++ test/unit/test_santander.py
@@ -12,77 +12,63 @@ def disabled(f):
 
 class TestUnit(unittest.TestCase):
 
-    def testParse(self):
-
-        inp = \
+    def setUp(self):
+        self.rawData = \
         [
-        "From: 31/12/2011 to 31/12/2012\n",
-        "\n",
-        "Account: XXXX XXXX XXXX XXXX\n",
-        "\n",
-        "Date: 29/12/2012\n",
-        "Description: CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, RATE 1.00/GBP ON 26-12-2012\n",
-        "Amount: -10.45 GBP\n",
-        "Balance: 3472.63 GBP\n",
-        "\n",
-        "Date: 28/12/2012\n",
-        "Description: CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, RATE 1.00/GBP ON 26-12-2012\n",
-        "Amount: -10.00\n",
-        "Balance: 3483.08 GBP"
+            "From: 31/12/2011 to 31/12/2012\n",
+            "\n",
+            "Account: XXXX XXXX XXXX XXXX\n",
+            "\n",
+            "Date: 29/12/2012\n",
+            "Description: CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, RATE 1.00/GBP ON 26-12-2012\n",
+            "Amount: -10.45 GBP\n",
+            "Balance: 3472.63 GBP\n",
+            "\n",
+            "Date: 28/12/2012\n",
+            "Description: CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, RATE 1.00/GBP ON 26-12-2012\n",
+            "Amount: -10.00\n",
+            "Balance: 3483.08 GBP"
         ]
 
-        HEADER = ["DATE", "DESCRIPTION", "AMOUNT", "BALANCE"]
-        BODY = \
+        header = ["DATE", "DESCRIPTION", "AMOUNT", "BALANCE"]
+        parsedBody = \
         [
             ["29/12/2012",
-            "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-            "RATE 1.00/GBP ON 26-12-2012",
-            "-10.45",
-            "3472.63"],
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012",
+             "-10.45",
+             "3472.63"],
             ["28/12/2012",
-            "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-            "RATE 1.00/GBP ON 26-12-2012",
-            "-10.00",
-            "3483.08"]
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012",
+             "-10.00",
+             "3483.08"]
         ]
-        out = pd.DataFrame(BODY, columns=HEADER)
+        swappedBody = \
+        [
+            ["29/12/2012",
+             "3472.63",
+             "-10.45",
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012"],
+            ["28/12/2012",
+             "3483.08",
+             "-10.00",
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012"]
+        ]
 
-        if not out.equals(san.parse(inp)):
+        self.dfParsed = pd.DataFrame(parsedBody, columns=header)
+        self.dfSwapped = pd.DataFrame(swappedBody, columns=header)
+
+    def testParse(self):
+        if not self.dfParsed.equals(san.parse(self.rawData)):
             self.fail("The DataFrames are not equal!")
 
     def testSwapColumns(self):
+       if not self.dfSwapped.equals(san.swapColumns(self.dfParsed)):
+           self.fail("The DataFrames are not equal!")
 
-        inp = \
-        {
-            0: ["DATE", "DESCRIPTION", "AMOUNT", "BALANCE"],
-            1: ["29/12/2012",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012",
-                "-10.45",
-                "3472.63"],
-            2: ["28/12/2012",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012",
-                "-10.00",
-                "3483.08"]
-        }
-
-        out = \
-        {
-            0: ["DATE", "BALANCE", "AMOUNT", "DESCRIPTION"],
-            1: ["29/12/2012",
-                "3472.63",
-                "-10.45",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012"],
-            2: ["28/12/2012",
-                "3483.08",
-                "-10.00",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012"]
-        }
-
-        self.assertDictEqual(san.swapColumns(inp), out)
 
 
 class TestIntegration(unittest.TestCase):
