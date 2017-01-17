78b00855bb611938f59e6bcc7fcaf5e51588d0e2
OthmanEmpire <oz.alikhan@gmail.com>
2017-01-17 08:01:46 +0000

    Removed 'extractPlotData' as plotting can be direct with DataFrame

--- monies/visualise.py
+++ monies/visualise.py
@@ -2,7 +2,6 @@
 A script responsible for visualising money expenditure from bank statements.
 """
 import math
-import datetime
 
 import numpy as np
 import pandas as pd
@@ -22,28 +21,6 @@ def query(entries, keyWord):
     return entries[entries["DESCRIPTION"].str.contains(keyWord, na=False)]
 
 
-def extractPlotData(entries):
-    """
-    Converts the given entries into a suitable form so that it can be plotted
-    directly using matplotlib functions.
-
-    :param entries: A dictionary mapping line numbers to transaction entries.
-    :return: A tuple containing DATE and AMOUNT entries as tuples.
-    """
-    data = []
-    entries.pop(0)      # Removes header
-
-    for line, entry in entries.items():
-        d, _, a, _ = entry
-
-        point = (datetime.datetime.strptime(d, "%d/%m/%Y"), float(a))
-        data.append(point)
-
-    data = sorted(data, key=lambda x: x[0])     # Sorts based on dates
-    dates, amounts = zip(*data)
-    return dates, amounts
-
-
 def search(categories, entries):
     """
     Searches the entries for the given categories.
@@ -60,23 +37,6 @@ def search(categories, entries):
         found.append(f)
 
     return found
-
-
-def write(entries, fPath):
-    """
-    Writes the given data which contains transaction entries into a text
-    file in CSV format.
-
-    :param entries: A dictionary mapping line numbers to transaction entries.
-    :param fPath: The path to the output file.
-    """
-    with open(fPath, "wb") as outF:
-
-        # Writing data using numpy
-        entries = [entry for num, entry in entries.items()]
-        entries = np.array(entries)
-        dFormat = "%10s {0} %9s {0} %8s {0} %s".format(3*" ")
-        np.savetxt(outF, np.array(entries), fmt=dFormat)
 
 
 def plotTimeGraph(data):
@@ -119,6 +79,23 @@ def plotTimeGraph(data):
 
     fig.tight_layout()
     plt.show()
+
+
+def write(entries, fPath):
+    """
+    Writes the given data which contains transaction entries into a text
+    file in CSV format.
+
+    :param entries: A dictionary mapping line numbers to transaction entries.
+    :param fPath: The path to the output file.
+    """
+    with open(fPath, "wb") as outF:
+
+        # Writing data using numpy
+        entries = [entry for num, entry in entries.items()]
+        entries = np.array(entries)
+        dFormat = "%10s {0} %9s {0} %8s {0} %s".format(3*" ")
+        np.savetxt(outF, np.array(entries), fmt=dFormat)
 
 
 def main():
--- test/unit/test_visualise.py
+++ test/unit/test_visualise.py
@@ -50,8 +50,39 @@ class TestUnit(unittest.TestCase):
              "RATE 1.00/GBP ON 26-12-2012"],
         ]
 
+        plotBody = \
+        [
+            ["DATE", "BALANCE", "AMOUNT", "DESCRIPTION"],
+            ["29/12/2012",
+             "3472.63",
+             "-10.45",
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012"],
+            ["28/12/2012",
+             "3483.08",
+             "-10.00",
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012"],
+            ["27/12/2012",
+             "3483.08",
+             "-14.00",
+             "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
+             "RATE 1.00/GBP ON 26-12-2012"],
+        ]
+
+        self.PlotData = \
+        (
+            (datetime.datetime(2012, 12, 27, 0, 0),
+             datetime.datetime(2012, 12, 28, 0, 0),
+             datetime.datetime(2012, 12, 29, 0, 0)),
+            (-14.00,
+             -10.00,
+             -10.45)
+        )
+
         self.dfInp = pd.DataFrame(body, columns=header)
         self.dfQuery = pd.DataFrame(queryBody, columns=header)
+        self.dfPlot = pd.DataFrame(plotBody, columns=header)
 
     def testQuery(self):
         exp = self.dfQuery
@@ -60,38 +91,6 @@ class TestUnit(unittest.TestCase):
         if not out.equals(exp):
             self.fail("EXPECTED:\n{}\n\n\n"
                       "ACTUAL:\n{}\n\n\n".format(exp, out))
-
-    @disabled
-    def testExtractPlotData(self):
-
-        inp = \
-        {
-            0: ["DATE", "BALANCE", "AMOUNT", "DESCRIPTION"],
-            1: ["29/12/2012",
-                "3472.63",
-                "-10.45",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012"],
-            2: ["28/12/2012",
-                "3483.08",
-                "-10.00",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012"],
-            3: ["27/12/2012",
-                "3483.08",
-                "-14.00",
-                "CARD PAYMENT TO WWW.JUST EAT.CO.UK,10.45 GBP, "
-                "RATE 1.00/GBP ON 26-12-2012"],
-        }
-
-        out = ((datetime.datetime(2012, 12, 27, 0, 0),
-                datetime.datetime(2012, 12, 28, 0, 0),
-                datetime.datetime(2012, 12, 29, 0, 0)),
-               (-14.00,
-                -10.00,
-                -10.45))
-
-        self.assertTupleEqual(vis.extractPlotData(inp), out)
 
     @disabled
     def testSearch(self):
